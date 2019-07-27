9f57d06bbc79458bcf14eba5e0388c7d7a6f8301
Ajay Krishna reddy <ajay.krishna@iwebber.co.in>
2019-07-27 15:46:08 +0530

    custom service provider added

---
+++ app/Library/Services/DemoOne.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Library\Services;
+
+class DemoOne {
+    public function getName(){
+        return 'Ajay Krishna Reddy';
+    }
+
+    public function getStringLength($str){
+       return strlen($str);
+    }
+}
